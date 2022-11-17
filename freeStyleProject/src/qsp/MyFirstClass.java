73fe5e946204f9118bf1f43cb6594a6223930161
shahwaj1998 <118388746+shahwaj1998@users.noreply.github.com>
2022-11-17 07:20:38 +0530

    Update MyFirstClass.java

--- freeStyleProject/src/qsp/MyFirstClass.java
+++ freeStyleProject/src/qsp/MyFirstClass.java
@@ -2,7 +2,7 @@ package qsp;
 
 public class MyFirstClass {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world, How are you?!");
 	}
 
 }
